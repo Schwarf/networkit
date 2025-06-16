6b3cea75f4e57a759db289fcfb35a76c01d7a57b
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-06-16 19:29:54 +0200

    Add files.

---
+++ include/networkit/flow/NetworkSimplex.hpp
@@ -0,0 +1,28 @@
+/*  NetworkSimplex.hpp
+*
+ *	Created on: 16.06.2025
+ *  Authors: Andreas Scharf (andreas.b.scharf@gmail.com)
+ *
+ */
+
+#ifndef NETWORKIT_FLOW_NETWORKSIMPLEX_HPP_
+#define NETWORKIT_FLOW_NETWORKSIMPLEX_HPP_
+#include <networkit/graph/Graph.hpp>
+#include <string>
+#include <vector>
+#include <utility>
+
+namespace NetworKit {
+class NetworkSimplex {
+public:
+    NetworkSimplex(const Graph &G,
+                   const std::string& demandAttributeName   = "demand",
+                   const std::string& capacityAttributName  = "capacity",
+                   const std::string& weightAttributeName   = "weight");
+
+    std::pair<double,std::vector<double>> run();
+
+    // …
+};
+}
+#endif //NETWORKSIMPLEX_H
---
+++ networkit/cpp/flow/NetworkSimplex.cpp
@@ -0,0 +1,6 @@
+/*  NetworkSimplex.cpp
+*
+ *	Created on: 16.06.2025
+ *  Authors: Andreas Scharf (andreas.b.scharf@gmail.com)
+ *
+ */
