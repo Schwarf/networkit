141f6f40810bd24d22799d83f136afb4683f5a97
Andreas Scharf <andreas.b.scharf@gmail.com>
2025-12-17 20:03:27 +0100

    Add initialization kernel.

--- include/networkit/gpu/SSSPKernels.cuh
+++ include/networkit/gpu/SSSPKernels.cuh
@@ -1,5 +1,5 @@
 /*  SSSPKernels.cuh
-*
+ *
  *  Created on: 17.12.2025
  *  Authors: Andreas Scharf (andreas.b.scharf@gmail.com)
  *
@@ -7,11 +7,30 @@
 #ifndef NETWORKIT_GPU_SSSP_KERNELS_CUH_
 #define NETWORKIT_GPU_SSSP_KERNELS_CUH_
 
-#include <cuda_runtime.h>
 #include <cstdint>
-
+#include <cuda_runtime.h>
+#include <math_constants.h>
 
 namespace NetworKit::GPU {
+
+template <typename node_t, typename WeightT>
+__global__ void initDistancesAndFrontierKernel(WeightT *distances, node_t numberOfNodes, node_t src,
+                                               node_t *frontier, std::uint32_t *frontierCount) {
+
+    const node_t tid = static_cast<node_t>(blockIdx.x) * static_cast<node_t>(blockDim.x)
+                       + static_cast<node_t>(threadIdx.x);
+
+    if (tid < numberOfNodes) {
+        distances[tid] = CUDART_INF_F;
+    }
+
+    // single-thread initialization
+    if (tid == 0) {
+        distances[src] = static_cast<WeightT>(0);
+        frontier[0] = src;
+        *frontierCount = 1;
+    }
+}
 
 __device__ inline float atomicMinFloat(float *address, float value) {
     int *addressAsInt = reinterpret_cast<int *>(address);
@@ -29,22 +48,21 @@ __device__ inline float atomicMinFloat(float *address, float value) {
 }
 
 template <typename IndexT, typename NodeT>
-__global__ void relaxFromFrontierKernel(const IndexT* __restrict__ rowPointer,
-                                        const NodeT*  __restrict__ columnIndices,
-                                        const float*  __restrict__ weights,
-                                        float* __restrict__ dist,
-                                        const NodeT* __restrict__ currentFrontier,
-                                        std::uint32_t currentCount,
-                                        NodeT* __restrict__ nextFrontier,
-                                        std::uint32_t* __restrict__ nextCount) {
+__global__ void
+relaxFromFrontierKernel(const IndexT *__restrict__ rowPointer,
+                        const NodeT *__restrict__ columnIndices, const float *__restrict__ weights,
+                        float *__restrict__ dist, const NodeT *__restrict__ currentFrontier,
+                        std::uint32_t currentCount, NodeT *__restrict__ nextFrontier,
+                        std::uint32_t *__restrict__ nextCount) {
     const std::uint32_t tid = blockIdx.x * blockDim.x + threadIdx.x;
-    if (tid >= currentCount) return;
+    if (tid >= currentCount)
+        return;
 
     const NodeT u = currentFrontier[tid];
     const float du = dist[u];
 
     const IndexT begin = rowPointer[u];
-    const IndexT end   = rowPointer[u + 1];
+    const IndexT end = rowPointer[u + 1];
 
     for (IndexT e = begin; e < end; ++e) {
         const NodeT v = columnIndices[e];
