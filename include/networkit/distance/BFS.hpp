79d44861296304610e2882f014691e81fa107914
Andreas Scharf <andreas.b.scharf@gmail.com>
2026-05-17 09:47:09 +0200

    Adapt BFS.

--- include/networkit/distance/BFS.hpp
+++ include/networkit/distance/BFS.hpp
@@ -8,6 +8,9 @@
 #ifndef NETWORKIT_DISTANCE_BFS_HPP_
 #define NETWORKIT_DISTANCE_BFS_HPP_
 
+#include <limits>
+#include <queue>
+
 #include <networkit/distance/SSSP.hpp>
 
 namespace NetworKit {
@@ -17,7 +20,10 @@ namespace NetworKit {
  * The BFS class is used to do a breadth-first search on a Graph from a given
  * source node.
  */
-class BFS final : public SSSP {
+template <typename GraphType>
+class BFSBase final : public SSSPBase<GraphType> {
+    using NodeType = typename GraphType::node_type;
+    using EdgeWeightType = typename GraphType::edge_weight_type;
 
 public:
     /**
@@ -31,13 +37,83 @@ public:
      * increasing distance from the source.
      * @param target The target node.
      */
-    BFS(const Graph &G, node source, bool storePaths = true,
-        bool storeNodesSortedByDistance = false, node target = none);
+    BFSBase(const GraphType &G, NodeType source, bool storePaths = true,
+            bool storeNodesSortedByDistance = false,
+            NodeType target = std::numeric_limits<NodeType>::max());
 
     /**
      * Breadth-first search from @a source.
      */
     void run() override;
 };
+
+template <typename GraphType>
+BFSBase<GraphType>::BFSBase(const GraphType &G, NodeType source, bool storePaths,
+                            bool storeNodesSortedByDistance, NodeType target)
+    : SSSPBase<GraphType>(G, source, storePaths, storeNodesSortedByDistance, target) {}
+
+template <typename GraphType>
+void BFSBase<GraphType>::run() {
+    const count z = this->G->upperNodeIdBound();
+    this->reachedNodes = 1;
+    this->sumDist = 0.;
+
+    const auto infDist = std::numeric_limits<EdgeWeightType>::max();
+    std::fill(this->distances.begin(), this->distances.end(), infDist);
+
+    if (this->distances.size() < z)
+        this->distances.resize(z, infDist);
+
+    if (this->storePaths) {
+        this->previous.clear();
+        this->previous.resize(z);
+        this->npaths.clear();
+        this->npaths.resize(z, 0);
+        this->npaths[this->source] = 1;
+    }
+
+    if (this->storeNodesSortedByDistance) {
+        std::vector<NodeType> empty;
+        std::swap(this->nodesSortedByDistance, empty);
+    }
+
+    std::queue<NodeType> q;
+    q.push(this->source);
+    this->distances[this->source] = 0.;
+
+    const bool breakWhenFound = (this->target != std::numeric_limits<NodeType>::max());
+    while (!q.empty()) {
+        NodeType u = q.front();
+        q.pop();
+
+        if (this->storeNodesSortedByDistance) {
+            this->nodesSortedByDistance.push_back(u);
+        }
+        if (breakWhenFound && this->target == u) {
+            break;
+        }
+
+        // Insert untouched neighbors into queue.
+        this->G->forNeighborsOf(u, [&](NodeType v) {
+            if (this->distances[v] == infDist) {
+                q.push(v);
+                this->distances[v] = this->distances[u] + 1.;
+                this->sumDist += this->distances[v];
+                ++this->reachedNodes;
+                if (this->storePaths) {
+                    this->previous[v] = {u};
+                    this->npaths[v] = this->npaths[u];
+                }
+            } else if (this->storePaths && (this->distances[v] == this->distances[u] + 1.)) {
+                this->previous[v].push_back(u);
+                this->npaths[v] += this->npaths[u];
+            }
+        });
+    }
+
+    this->hasRun = true;
+}
+
+using BFS = BFSBase<Graph>;
 } /* namespace NetworKit */
 #endif // NETWORKIT_DISTANCE_BFS_HPP_
